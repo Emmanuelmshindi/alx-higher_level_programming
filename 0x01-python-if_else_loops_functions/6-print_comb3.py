c19be6b36f2035d3be240ace4bc66d0455e5ff20
Emmanuelmshindi <mshindimanuel@gmail.com>
2023-01-05 07:14:26 +0000

    Printcomb2

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 90):
-    if i % 10 > i / 10:
-        if i != 89:
-            print(f"{i:02d} ", end='')
+for i in range(0, 9):
+    for j in range(i + 1, 10):
+        if i == 8:
+            print("{}{}".format(i, j))
         else:
-            print(f"{i:02d}")
+            print("{}{}".format(i, j), end=", ")
