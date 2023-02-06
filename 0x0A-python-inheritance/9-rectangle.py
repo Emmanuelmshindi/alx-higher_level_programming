bc4ced5e3a8fada12341a25d5fcce2a7ae51630e
Emmanuelmshindi <mshindimanuel@gmail.com>
2023-02-06 15:27:08 -0800

    Task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -6,7 +6,7 @@ BaseGeometry = __import__("7-base_geometry").BaseGeometry
 class Rectangle(BaseGeometry):
     """Rectangle class"""
     def __init__(self, width, height):
-        """Initialize rectangle method"""
+        """Initilize rectangle method"""
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
@@ -18,4 +18,4 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """Returns a string"""
-        print("[Rectangle] {}/{}".format(self.__width, self.__height)
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
