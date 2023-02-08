1770a5ca8121fa555f233b256002472156f99b9d
Emmanuelmshindi <mshindimanuel@gmail.com>
2023-02-08 00:41:12 -0800

    Task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,45 +1,32 @@
 #!/usr/bin/python3
-"""Module defining the class Student based on 9-student.py"""
+"""Defines a class Student."""
 
 
 class Student:
-    """
-    Class that defines properties of student.
+    """Represent a student."""
 
-    Attributes:
-        first_name (str): first name of student.
-        last_name (int): last name of student.
-        age (int): age of student.
-    """
     def __init__(self, first_name, last_name, age):
-        """Creates new instances of Student.
+        """Initialize a new Student.
 
         Args:
-            first_name (str): first name of student.
-            last_name (int): last name of student.
-            age (int): age of student.
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves a dictionary representation of a Student instance.
+        """Get a dictionary representation of the Student.
 
-        If attrs is a list of strings, only attribute names contained in,
-        this list must be retrieved.
-        Otherwise, all attributes must be retrieved.
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
 
-        Returns:
-            dict: dictionary representation.
+        Args:
+            attrs (list): (Optional) The attributes to represent.
         """
-        if attrs is None:
-            return self.__dict__
-
-        new_dict = {}
-        for item in attrs:
-            try:
-                new_dict[item] = self.__dict__[item]
-            except exception:
-                pass
-            return new_dict
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
