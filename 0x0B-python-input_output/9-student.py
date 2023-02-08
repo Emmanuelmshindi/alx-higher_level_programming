ae80a970a27a68b94b28f13603461f2d8f9d87b9
Emmanuelmshindi <mshindimanuel@gmail.com>
2023-02-08 00:32:57 -0800

    Task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3
-"""Module defining the class Student"""
+"""Defines a class Student."""
 
 
 class Student:
-    """
-    Class that defines properties of student.
+    """Represents a student."""
 
-    Attributes:
-    first_name (str): first name of student.
-    last_name (int): last name of student.
-    age (int): age of student.
-    """
     def __init__(self, first_name, last_name, age):
-        """Creates new instances of Student.
+        """Initialize a new Student.
 
-        Args:                                                                               first_name (str): first name of student.
-                                                                                            last_name (int): last name of student.
-                                                                                            age (int): age of student.
-                                                                                        """                                                                             self.first_name = first_name
-                                                                                        self.last_name = last_name
-                                                                                        self.age = age
-                                                                                    def to_json(self):                                                                  """Retrieves a dictionary representation of a Student instance.
-        Returns:
-            dict: dictionary representation.                                            """
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Get a dictionary representation of the Student."""
         return self.__dict__
