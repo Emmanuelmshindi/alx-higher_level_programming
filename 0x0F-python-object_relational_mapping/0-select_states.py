da18741d3d4eb5a975ba365d8a7eac00264d51e0
Emmanuelmshindi <mshindimanuel@gmail.com>
2023-04-15 08:09:07 -0700

    Initial commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+This script lists all states in a MySQL database from a table called 'states'
+The MySQL username, password and database name are passed as arguments in the
+command line.
+The results are sorted by id in ascending order and printed out on the
+command line.The module MySQLdb is used to connect to a MySQL server on
+localhost on port 3306.A select statement is used to fetch all rows from the
+states table, alongside the fetchall() function, and prints the output in
+the format '(id, name)'
+Usage:
+    ./0-select_states.py username password mydatabase
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+            passwd=password, db=database)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+    db.close()
