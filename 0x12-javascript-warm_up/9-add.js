f104e7177dea37152111c199bd40444881b4809f
Emmanuelmshindi <mshindimanuel@gmail.com>
2023-08-16 12:38:11 +0000

    Task 9 revise

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,15 @@
 #!usr/bin/node
-const a = parseInt(process.argv[2]);
-const b = parseInt(process.argv[3]);
 
 function add(a, b) {
   const sum = a+b;
   console.log(sum);
+}
+
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+if(isNaN(a) || isNaN(b)) {
+  console.log("NaN")
+} else {
+  add(a, b);
 }
