2375934b0c7873283a97dda449a37e42c9f4d5f8
Emmanuelmshindi <mshindimanuel@gmail.com>
2023-08-30 12:01:02 +0000

    Task2 rev

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,20 +10,17 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-            passwd=password, db=database)
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2], 
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
 
     cur = db.cursor()
 
-    cur.execute("TRUNCATE TABLE states")
-
-    cur.execute(open("0-select_states.sql", "r").read())
-
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states\ 
+                 WHERE name LIKE BINARY 'N%'\
+                 ORDER BY id ASC")
 
     query_rows = cur.fetchall()
 
