d95a585ae6c3549dc64339b77ce8c527700fa171
Emmanuelmshindi <mshindimanuel@gmail.com>
2023-08-30 20:05:39 +0000

    Task rev

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -24,6 +24,6 @@ if __name__ == '__main__':
     states = session.query(State).filter.(State.name.like('%a%')).all()
 
     for state in states:
-        print("{}: {}".format(state.id, state.name)
+        print("{}: {}".format(states.id, states.name)
 
     session.close()
