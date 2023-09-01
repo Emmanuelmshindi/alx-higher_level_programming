3a7c3d6d837708a714ae561632e8d6dfb1287ecd
Emmanuelmshindi <mshindimanuel@gmail.com>
2023-09-01 05:14:19 +0000

    Task 1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # Send a get request to a url given as an argument.Print body size
-
-url="$1"
-
-size=$(echo curl -sl "$url" | grep -i 'content-length' | awk '{print $2}')
-
-echo "$size"
+curl -s "$1" | wc -c
