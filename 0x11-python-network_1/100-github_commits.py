24d788725bb648ad18029f6e7a9d5db66073cb7c
Emmanuelmshindi <mshindimanuel@gmail.com>
2023-09-02 14:03:16 -0700

    advanced task

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    Python script to list 10 commits of the repository
+"""
+import sys
+import requests
+
+if __name__ == '__main__':
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+    r = requests.get(url)
+    data = r.json()
+    for commit in data[:10]:
+        sha = commit['sha']
+        name = commit['commit']['author']['name']
+        print("{} {}".format(sha, name))
