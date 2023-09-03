c0507510948ebcd7f01ac0af8d231370b8767071
Emmanuelmshindi <mshindimanuel@gmail.com>
2023-09-03 19:51:38 +0000

    Advanced 3

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const file1Path = process.argv[2];
+const file2Path = process.argv[3];
+const outputPath = process.argv[4];
+
+fs.readFile(file1Path, 'utf8', (err, data1) => {
+  if (err) {
+    console.error(`Error reading ${file1Path}: ${err}`);
+    return;
+  }
+
+  fs.readFile(file2Path, 'utf8', (err, data2) => {
+    if (err) {
+      console.error(`Error reading ${file2Path}: ${err}`);
+      return;
+    }
+
+    const concatData = data1 + data2;
+    fs.writeFile(outputPath, concatData, 'utf8', (err) => {
+      if (err) {
+        console.error(`Error writing to ${outputPath}: ${err}`);
+        return;
+      }
+
+      console.log(`${concatData}`);
+    });
+  });
+});
