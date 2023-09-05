2eb18d06e9f8f78f8f2f6d3ae4c1dc48fa3a7f0a
Emmanuelmshindi <mshindimanuel@gmail.com>
2023-09-04 23:08:50 -0700

    Task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,20 +1,6 @@
 #!/usr/bin/node
-
-/**
-* a script to compute the factorial of a number
-**/
-
-function fact(a) {
-if (isNaN(a)) {
-  return (1);
-} else if (a == 0 || a == 1) {
-  return (1);
-} else {
-  return (a * fact(a - 1));
-}
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
 
-const input = parseInt(process.argv[2]);
-const res = fact(input);
-
-console.log(res);
+console.log(factorial(Number(process.argv[2])));
